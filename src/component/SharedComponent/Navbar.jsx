9016e9dfeff1ed4828479af5fa714d6f1d2e4c8f
motiur-rahman-dhrubo <mdmotiurrahman383@gmail.com>
2025-03-14 12:13:49 +0600

    ridoy vai's code maching

--- src/component/SharedComponent/Navbar.jsx
+++ src/component/SharedComponent/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { Link, NavLink } from "react-router";
 import { AuthContext } from "../../providers/AuthProvider";
-
+import logo from "/CB-logo.png";
 export default function Navbar() {
   const { user, logOut } = useContext(AuthContext);
   const handleSignOut = () => {
@@ -13,17 +13,19 @@ export default function Navbar() {
   };
   const navLink = (
     <>
-      {" "}
       <li>
-        <NavLink to={"login"}>Login</NavLink>
+        <NavLink to={"/"}>Home</NavLink>
+      </li>
+      {/* <li>
+        <NavLink to={"/all-jobs"}>All Jobs</NavLink>
       </li>
       <li>
-        <NavLink to={"register"}>Register</NavLink>
-      </li>
+        <NavLink to={"/about-us"}>About Us</NavLink>
+      </li> */}
     </>
   );
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar w-11/12 mx-auto text-white">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -34,25 +36,28 @@ export default function Navbar() {
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
-              {" "}
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
                 d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
+              />
             </svg>
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
             {navLink}
           </ul>
         </div>
-        <NavLink to={"/"} className="btn btn-ghost text-xl">
-          Chakri-Bakri
-        </NavLink>
+        <Link to={"/"} className="w-16 rounded-lg">
+          <img
+            src={logo}
+            className="w-full rounded-lg"
+            alt="Chakri_Bakri.logo"
+          />
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{navLink}</ul>
