b8c242e8e55e1a5fe2d18df9a8e4f3a3e212804f
motiur-rahman-dhrubo <mdmotiurrahman383@gmail.com>
2025-03-21 03:04:28 +0600

    ridoy vai's code marge

--- src/layout/DashboardLayout/Dashboard.jsx
+++ src/layout/DashboardLayout/Dashboard.jsx
@@ -16,8 +16,8 @@ import { MdFavoriteBorder } from "react-icons/md";
 const Dashboard = () => {
   const role = {
     isAdmin: false,
-    isJobPublisher: false,
-    isJobSeeker: true,
+    isJobPublisher: true,
+    isJobSeeker: false,
   };
 
 
