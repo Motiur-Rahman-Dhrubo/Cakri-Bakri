38e2390b37942724fe1b31ac59bd08e47973446d
mahmud819 <mahmudurr819@gmail.com>
2025-03-23 05:30:16 +0600

    myprofile route add

--- src/main.jsx
+++ src/main.jsx
@@ -19,6 +19,7 @@ import DashboardOverview from "./pages/DashboardPages/Overview";
 import ManageUsers from "./pages/DashboardPages/ManageUser";
 import AppliedJobs from "./pages/AppliedJobs/AppliedJobs";
 import FavoriteJobs from "./pages/FavoriteJobs/FavoriteJobs";
+import Myprofile from "./pages/Myprofile/Myprofile";
 const root = document.getElementById("root");
 const queryClient = new QueryClient();
 ReactDOM.createRoot(root).render(
@@ -37,6 +38,7 @@ ReactDOM.createRoot(root).render(
           <Route path="/dashboard" element={<Dashboard />}>
             <Route path="/dashboard/overview" element={<DashboardOverview />} />
             <Route path="/dashboard/manage-users" element={<ManageUsers />} />
+            <Route path="/dashboard/myprofile" element={<Myprofile/>} />
             <Route path="/dashboard/applied-Jobs" element={<AppliedJobs />} />
             <Route path="/dashboard/favorite-Jobs" element={<FavoriteJobs />} />
           </Route>
---
+++ src/pages/Myprofile/Myprofile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Myprofile = () => {
+    return (
+        <div>
+            this is profile
+        </div>
+    );
+};
+
+export default Myprofile;
