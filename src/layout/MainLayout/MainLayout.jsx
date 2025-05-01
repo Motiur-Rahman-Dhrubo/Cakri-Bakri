79fab96af006674848a327deb8186aed11eb9e77
Md-Rabbi-Sarkar <noreply@github.com>
2025-05-01 22:33:01 +0600

    make responsiv

--- src/layout/MainLayout/MainLayout.jsx
+++ src/layout/MainLayout/MainLayout.jsx
@@ -6,13 +6,13 @@ import Navbar from "../../component/SharedComponent/Navbar";
 const MainLayout = () => {
   return (
     <>
-      <header className="bg-gradient-to-r from-[#175A87] via-[#177C87] to-[#64CCC5] shadow-2xl">
+      <header className="bg-gradient-to-r from-[#175A87] via-[#177C87] to-[#64CCC5] shadow-sm">
         <Navbar />
       </header>
       <main className="bg-[#EEF5FF] min-h-[calc(100vh-337px)]">
         <Outlet></Outlet>
       </main>
-      <footer className="bg-cb-btn ">
+      <footer className="bg-cb-secondary ">
         <Footer />
       </footer>
     </>
