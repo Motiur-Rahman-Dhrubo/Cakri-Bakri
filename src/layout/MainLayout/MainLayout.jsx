c2d5e622d7a094684a8ff9583af4e9ba0e8fe646
Md-Rabbi-Sarkar <noreply@github.com>
2025-05-01 22:32:27 +0600

    fixed shadow

--- src/layout/MainLayout/MainLayout.jsx
+++ src/layout/MainLayout/MainLayout.jsx
@@ -6,7 +6,7 @@ import Navbar from "../../component/SharedComponent/Navbar";
 const MainLayout = () => {
   return (
     <>
-      <header className="bg-gradient-to-r from-[#175A87] via-[#177C87] to-[#64CCC5] shadow-sm">
+      <header className="bg-gradient-to-r from-[#175A87] via-[#177C87] to-[#64CCC5] shadow-2xl">
         <Navbar />
       </header>
       <main className="bg-[#EEF5FF] min-h-[calc(100vh-337px)]">
