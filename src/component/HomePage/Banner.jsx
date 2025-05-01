ecc36ee40d2d2e04763faa36730afa9afe0ec233
Md-Rabbi-Sarkar <noreply@github.com>
2025-05-01 22:19:47 +0600

    make responsiv bannar

--- src/component/HomePage/Banner.jsx
+++ src/component/HomePage/Banner.jsx
@@ -13,12 +13,12 @@ export default function Banner() {
   };
   return (
     <section
-      className={`bg-[url('/hero-bg.png')] bg-cover bg-no-repeat bg-center w-2/3 md:h-[500px]`}
+      className={`bg-[url('/hero-bg.png')] bg-cover bg-no-repeat bg-center w-full md:h-[500px]`}
     >
       <div className="w-11/12 h-full mx-auto grid grid-cols-2 content-center">
         <div className="justify-self-start">
           <h1 className="text-5xl md:text-6xl font-primary text-gray-800 uppercase font-bold">
-            Chakri <span className="text-cb-btn">Bakri</span>
+            Chakri <span className="text-cb-primary">Bakri</span>
           </h1>
           <p className="mt-4 text-lg text-gray-400/95 md:w-[450px]">
             Find Your Dream Job. Connect with Top Employers. A seamless platform
