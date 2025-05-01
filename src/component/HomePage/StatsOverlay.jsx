33abc060ee58414b4c81fe664702cf921aae619a
limon <limonsk026@gmail.com>
2025-05-01 22:24:53 +0600

    change color in red for stats

--- src/component/HomePage/StatsOverlay.jsx
+++ src/component/HomePage/StatsOverlay.jsx
@@ -9,7 +9,7 @@ export default function StatsOverlay() {
       <div className="relative z-10 max-w-6xl mx-auto px-6 md:px-12 flex flex-wrap justify-between items-center text-center">
         <div className="w-1/2 md:w-1/4 py-4 transform transition-all duration-500 hover:scale-105 hover:drop-shadow-[0_4px_10px_rgba(4,164,217,0.8)]">
           <h2 className="text-3xl font-bold text-cb-white/90">12K+</h2>
-          <p className="text-xl font-semibold ">Jobs Available</p>
+          <p className="text-xl font-semibold text-cb-white/80">Jobs Available</p>
         </div>
         <div className="w-1/2 md:w-1/4 py-4 transform transition-all duration-500 hover:scale-105 hover:drop-shadow-[0_4px_10px_rgba(4,164,217,0.8)]">
           <h2 className="text-3xl font-bold text-cb-white/90">50K+</h2>
